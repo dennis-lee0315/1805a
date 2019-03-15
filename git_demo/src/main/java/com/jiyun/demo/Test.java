32a8b667e1bdcf8536fea139fe166039703e3865
dennis0315 <17600649060@163.com>
2019-03-15 16:58:47 +0800

    删除文件，提交java项目

---
+++ git_demo/src/main/java/com/jiyun/demo/Test.java
@@ -0,0 +1,9 @@
+package com.jiyun.demo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello svn");
+        System.out.println("测试svn代码提交");
+    }
+}
---
+++ git_demo/src/main/java/com/jiyun/demo/Test2.java
@@ -0,0 +1,8 @@
+package com.jiyun.demo;
+public class Test2{
+	
+	public static void main(String[] args){
+		System.out.println("svn test2");
+		System.out.println("svn test2");
+	}
+}
