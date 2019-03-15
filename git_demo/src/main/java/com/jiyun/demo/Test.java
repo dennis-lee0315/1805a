63635b956d568c495b14015e08b348683c591b5d
dennis0315 <17600649060@163.com>
2019-03-15 17:02:07 +0800

    删除先买个u

--- git_demo/src/main/java/com/jiyun/demo/Test.java
+++
@@ -1,9 +0,0 @@
-package com.jiyun.demo;
-
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("hello svn");
-        System.out.println("测试svn代码提交");
-    }
-}
